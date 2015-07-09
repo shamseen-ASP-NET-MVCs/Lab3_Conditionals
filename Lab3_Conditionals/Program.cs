ceb6b846b71cbe238c6aaa9b415198545ccf076e
shamseen <shamseen.c@gmail.com>
2015-07-09 14:43:04 -0400

    Fizzbuzz + Reverse characters + extra credit
    used for-loops on each exercise, limited by user input. also added
    additional comments to income tax calculator

--- Lab3_Conditionals/Program.cs
+++ Lab3_Conditionals/Program.cs
@@ -13,15 +13,25 @@ namespace Lab3_Conditionals
             //Income tax calculator
             Console.WriteLine("\n\n------INCOME TAX CALCULATOR-----");
             double tax = incomeTaxes();
-            if (tax != 0)
+
+            if (tax != -1) //will only be so if the input is invalid
                 Console.WriteLine("Your tax is {0:C}", tax);
 
-            ////Time and classifications
-            //Console.WriteLine("\n-----Time and Classifications-----");
-            //timeClassifications();
+            //Time and classifications
+            Console.WriteLine("\n\n-----TIME AND CLASSIFICATIONS-----");
+            timeClassifications();
 
-            
-        }
+            //FizzBuzz AGAIN
+            Console.WriteLine("\n\n-----FIZZBUZZ-----");
+            fizzBuzz();
+
+            //Reverse Characters
+            Console.WriteLine("\n\n-----REVERSE CHARACTERS------");
+            reverse();
+
+            //Extra Credit: Reverse Array of Strings
+            Console.WriteLine("\n\n-----EXTRA CREDIT-----");
+            extraCredit();        }
 
         static double incomeTaxes()
         {
@@ -37,7 +47,7 @@ namespace Lab3_Conditionals
             if (!Double.TryParse(input, out income))
             {
                 Console.WriteLine("****ERROR: Invalid input! Try again!*****");
-                return 0;
+                return -1;
             }
 
             //declaring variables needed for loop
@@ -66,9 +76,8 @@ namespace Lab3_Conditionals
 
             if (income > 75000)
                 tax += (income - 75000) * taxRates[length - 1];
-            
-            // return tax;
-             Console.WriteLine("Testing optimization: {0:C}", tax);
+
+            return tax;
 
             ///*************sloppy first attempt, but it works so it's my control**************/
             //tax = 0.0;
@@ -112,7 +121,6 @@ namespace Lab3_Conditionals
             //    }
             //}
             //Console.WriteLine("Control: {0:C}", tax); 
-            return tax; 
         }
 
         static void timeClassifications()
@@ -125,18 +133,80 @@ namespace Lab3_Conditionals
 
                 switch (seconds)
                 {
-                    case 0: Console.WriteLine("The new minute is starting!"); break;
-                    case 15: Console.WriteLine("We're one quarter done."); break;
-                    case 30: Console.WriteLine("Oooh, we're half. way. theere, OOOH LIVIN ON A PRAYER"); break;
-                    case 45: Console.WriteLine("Getting close to done."); break;
-                    default: Console.WriteLine(".. working on it .."); break;
+                    case 0: Console.Write("The new minute is starting!"); break;
+                    case 15: Console.Write("We're one quarter done."); break;
+                    case 30: Console.Write("Oooh, we're half. way. theere, OOOH LIVIN ON A PRAYER"); break;
+                    case 45: Console.Write("Getting close to done."); break;
+                    default: Console.Write(".. working on it .."); break;
                 }
 
                 //taking input to continue so i can keep testing
-                Console.Write("Continue? (Y/N) ");
+                Console.Write("\nContinue? (Y/N) ");
                 input = Console.ReadLine();
             }
+        }
 
+        static void fizzBuzz()
+        {
+            string input = "", output = "";
+            int limit = 0;
+
+            //taking input
+            Console.Write("How many numbers would you like to go to? ");
+            input = Console.ReadLine();
+
+            //giving an error if input wasn't a number
+            //otherwise setting it as the upper limit in for-loop
+            if (!Int32.TryParse(input, out limit))
+                Console.WriteLine("*****ERROR: Invalid input. Try again!*****");
+
+            for (int i = 1; i <= limit; i++)
+            {
+                if (i % 15 == 0)
+                    output = "FizzBuzz!";  //instead of printing string, just storing into output
+                else if (i % 3 == 0)
+                    output = "Fizz";
+                else if (i % 5 == 0)
+                    output = "Buzz";
+                else
+                    output = i.ToString();
+
+                Console.WriteLine(output); //printing out output.. i was bored.
+            }
+        }
+
+        static void reverse()
+        {
+            string input = "";
+
+            Console.WriteLine("Please enter word or sentence to reverse: ");
+            input = Console.ReadLine();
+
+            for(int i = input.Length - 1; i >= 0; i--)
+                Console.Write(input[i]);
+        }
+
+        static void extraCredit()
+        {
+            string input = "", word = "";
+        
+            //taking input
+            Console.Write("Please enter word or sentence to reverse: ");
+            input = Console.ReadLine();
+
+            string[] stringsArr = input.Split(); //putting each word of input into an array
+
+            for (int i = stringsArr.Length - 1; i >= 0; i--) //going through array in reverse
+            {
+                word = stringsArr[i];  //storing in a variable for readability in nested for-loop
+
+                for (int x = word.Length - 1; x >= 0; x--) //printing out characters in reverse
+                    Console.Write(word[x]);
+
+                Console.Write(" "); //space between words
+            }
+
+            Console.WriteLine();  //so that "Press any key to continue..." is on new line
         }
     }
 }
